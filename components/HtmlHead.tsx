794b0c64ab1f859373eb4bd4feb323825fc4e673
slify08 <joshxfi.dev@gmail.com>
2021-07-27 09:24:50 +0800

    add google-site-verification

--- components/HtmlHead.tsx
+++ components/HtmlHead.tsx
@@ -8,13 +8,13 @@ export const HtmlHead: React.FC = () => {
       <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       <meta
         name='description'
-        content='Hey there! My name is Josh Daniel Bañares, a React.js developer & enthusiast based in the Philippines.'
+        content="Hey there! My name is Josh Daniel Bañares, a React.js developer & enthusiast based in the Philippines. I'm a 17 year-old self-taught full-stack MERN developer. I constantly procrastinate & in discombobulation."
       />
       <meta name='robots' content='index, follow' />
       <meta name='author' content='Josh Daniel Bañares' />
       <meta
         name='google-site-verification'
-        content='XD3_-IfquEHcoYhlm_0vuGLh5ZTmS5XcAVgqLpL1f5s'
+        content='Enyu2q9yMt05fTlkT0JcCz3Z9tZA84VVPeBLui9XMyc'
       />
       <title>Josh Daniel</title>
     </Head>
