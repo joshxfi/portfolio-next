4c78c213a15b144760a0ff56ed1cd3bebc88cee6
slify08 <joshxfi.dev@gmail.com>
2021-07-27 16:07:27 +0800

    update: meta tag for seo

--- components/HtmlHead.tsx
+++ components/HtmlHead.tsx
@@ -4,23 +4,26 @@ import Head from "next/head";
 export const HtmlHead: React.FC = () => {
   return (
     <Head>
-      <meta charSet='UTF-8' />
-      <meta
-        name='viewport'
-        content='width=device-width, initial-scale=1.0, user-scalable=no'
-      />
-      <meta
-        name='description'
-        content="Hey there! My name is Josh Daniel Bañares, a React.js developer & enthusiast based in the Philippines. I'm a 17 year-old self-taught full-stack MERN developer. I constantly procrastinate & in discombobulation."
-      />
       <link rel='manifest' href='/site.webmanifest' />
-      <meta name='robots' content='index, follow' />
-      <meta name='author' content='Josh Daniel Bañares' />
+      <meta charSet='UTF-8' />
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       <meta
         name='google-site-verification'
         content='Enyu2q9yMt05fTlkT0JcCz3Z9tZA84VVPeBLui9XMyc'
       />
-      <title>Josh Daniel</title>
+      <meta name='title' content='Josh Daniel Bañares' />
+      <meta
+        name='description'
+        content="My name is Josh Daniel Bañares, a React.js developer & enthusiast based in the Philippines. I'm a 17 year-old self-taught developer."
+      />
+      <meta
+        name='keywords'
+        content='joshxfi, josh daniel bañares, xfi, 88xfi'
+      />
+      <meta name='robots' content='index, follow' />
+      <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+      <meta name='language' content='English' />
+      <meta name='author' content='Josh Daniel Bañares' />
     </Head>
   );
 };
