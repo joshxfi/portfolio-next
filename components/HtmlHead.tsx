10b7490d98f52cf36d86f29eacbe2de1f277b3cc
slify08 <joshxfi.dev@gmail.com>
2021-07-28 16:22:59 +0800

    add open graph markup

--- components/HtmlHead.tsx
+++ components/HtmlHead.tsx
@@ -11,19 +11,23 @@ export const HtmlHead: React.FC = () => {
         name='google-site-verification'
         content='Enyu2q9yMt05fTlkT0JcCz3Z9tZA84VVPeBLui9XMyc'
       />
-      <meta name='title' content='Josh Daniel Bañares' />
+      <meta name='title' content='Josh Daniel Bañares | Portfolio' />
       <meta
         name='description'
-        content="My name is Josh Daniel Bañares, a React.js developer & enthusiast based in the Philippines. I'm a 17 year-old self-taught developer."
-      />
-      <meta
-        name='keywords'
-        content='joshxfi, josh daniel bañares, xfi, 88xfi'
+        content='Portfolio by Josh Daniel Bañares, a 17 year-old self-taught React.js developer & enthusiast based in the Philippines.'
       />
       <meta name='robots' content='index, follow' />
       <meta httpEquiv='Content-Type' content='text/html; charset=utf-8' />
       <meta name='language' content='English' />
       <meta name='author' content='Josh Daniel Bañares' />
+
+      <meta property='og:title' content='Josh Daniel Bañares | Portfolio' />
+      <meta
+        property='og:description'
+        content='Portfolio by Josh Daniel Bañares, a 17 year-old self-taught React.js developer & enthusiast based in the Philippines.'
+      />
+      <meta property='og:type' content='website' />
+      <meta property='og:url' content='/thumbnail.jpg' />
       <title>Josh Daniel Bañares</title>
     </Head>
   );
