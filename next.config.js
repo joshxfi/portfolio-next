c5e7110c79dd3963d4ab73aecd849027d4686bd2
Josh Daniel <joshpersonal8@gmail.com>
2021-08-24 11:54:17 +0800

    Add redirects to external url

--- next.config.js
+++ next.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   reactStrictMode: true,
 }
+
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/github',
+        destination: 'https://github.com/joshxfi',
+        permanent: false,
+        basePath: false
+      },
+      
+      {
+        source: '/linkedin',
+        destination: 'https://www.linkedin.com/in/josh-daniel-ba%C3%B1ares/',
+        permanent: false,
+        basePath: false
+      },
+    ]
+  },
+};
